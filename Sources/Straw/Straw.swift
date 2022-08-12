de71ac2219dec2d627e16435b2c0aa8e8920af0a
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-08-12 14:36:56 -0500

    Initial import

--- Sources/Straw/Straw.swift
+++ Sources/Straw/Straw.swift
@@ -1,6 +1,136 @@
-public struct Straw {
-    public private(set) var text = "Hello, World!"
+//
+//  Straw.swift
+//  
+//
+//  Created by Dr. Brandon Wiley on 8/11/22.
+//
 
-    public init() {
+import Foundation
+
+public actor Straw
+{
+    var buffer: [Data] = []
+
+    public init()
+    {
     }
+
+    public func write(_ chunk: Data)
+    {
+        self.buffer.append(chunk)
+    }
+
+    public func write(_ chunks: [Data])
+    {
+        self.buffer.append(contentsOf: chunks)
+    }
+
+    public func read() throws -> Data
+    {
+        return self.buffer.removeFirst()
+    }
+
+    public func readAllChunks() throws -> [Data]
+    {
+        if self.buffer.isEmpty
+        {
+            throw StrawError.bufferEmpty
+        }
+
+        let result = self.buffer
+        self.buffer = []
+        return result
+    }
+
+    public func readAllData() throws -> Data
+    {
+        if self.buffer.isEmpty
+        {
+            throw StrawError.bufferEmpty
+        }
+
+        var result = Data()
+        for chunk in self.buffer
+        {
+            result.append(chunk)
+        }
+
+        self.buffer = []
+
+        return result
+    }
+
+    public func read(size: Int) throws -> Data
+    {
+        guard size > 0 else
+        {
+            throw StrawError.badReadSize(size)
+        }
+
+        let count = self.buffer.reduce(0) { $0 + $1.count }
+        guard count >= size else
+        {
+            throw StrawError.notEnoughBytes(size, count)
+        }
+
+        var result = Data()
+        while result.count < size
+        {
+            var chunk = self.buffer.removeFirst()
+            let bytesNeeded = size - result.count
+            if chunk.count <= bytesNeeded
+            {
+                result.append(chunk)
+            }
+            else // chunk.count > bytesNeeded
+            {
+                let bytes = chunk[0..<bytesNeeded]
+                result.append(bytes)
+                chunk = chunk[bytesNeeded...]
+                self.buffer.insert(chunk, at: 0)
+            }
+        }
+
+        return result
+    }
+
+    public func read(maxSize: Int) throws -> Data
+    {
+        guard maxSize > 0 else
+        {
+            throw StrawError.badReadSize(maxSize)
+        }
+
+        if self.buffer.isEmpty
+        {
+            throw StrawError.bufferEmpty
+        }
+
+        var result = Data()
+        while result.count < maxSize, !self.buffer.isEmpty
+        {
+            var chunk = self.buffer.removeFirst()
+            let bytesNeeded = maxSize - result.count
+            if chunk.count <= bytesNeeded
+            {
+                result.append(chunk)
+            }
+            else // chunk.count > bytesNeeded
+            {
+                let bytes = chunk[0..<bytesNeeded]
+                result.append(bytes)
+                chunk = chunk[bytesNeeded...]
+                self.buffer.insert(chunk, at: 0)
+            }
+        }
+
+        return result
+    }
+}
+
+public enum StrawError: Error
+{
+    case bufferEmpty
+    case badReadSize(Int)
+    case notEnoughBytes(Int, Int) // requested, actual
 }
--- Tests/StrawTests/StrawTests.swift
+++ Tests/StrawTests/StrawTests.swift
@@ -1,11 +1,101 @@
 import XCTest
 @testable import Straw
 
+import Datable
+
 final class StrawTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Straw().text, "Hello, World!")
+    func testRead() throws
+    {
+        let input = "test".data
+        let correct = input
+
+        Task
+        {
+            let straw = Straw()
+            await straw.write(input)
+            let result = try await straw.read()
+
+            XCTAssertEqual(result.string, correct.string)
+        }
+    }
+
+    func testRead2() throws
+    {
+        let input = "test".data
+        let correct = "testtest".data
+
+        Task
+        {
+            let straw = Straw()
+            await straw.write(input)
+            await straw.write(input)
+            let result = try await straw.read()
+
+            XCTAssertEqual(result.string, correct.string)
+        }
+    }
+
+    func testReadChunks() throws
+    {
+        let input = "test".data
+        let correct = [input, input]
+
+        Task
+        {
+            let straw = Straw()
+            await straw.write(input)
+            await straw.write(input)
+            let result = try await straw.readAllChunks()
+
+            XCTAssertEqual(result, correct)
+        }
+    }
+
+    func testReadSize() throws
+    {
+        let input = "test".data
+        let correct = "t".data
+
+        Task
+        {
+            let straw = Straw()
+            await straw.write(input)
+            await straw.write(input)
+            let result = try await straw.read(size: 1)
+
+            XCTAssertEqual(result.string, correct.string)
+        }
+    }
+
+    func testReadSize2() throws
+    {
+        let input = "test".data
+        let correct = "testt".data
+
+        Task
+        {
+            let straw = Straw()
+            await straw.write(input)
+            await straw.write(input)
+            let result = try await straw.read(size: 5)
+
+            XCTAssertEqual(result.string, correct.string)
+        }
+    }
+
+    func testReadMaxSize() throws
+    {
+        let input = "test".data
+        let correct = "testtest".data
+
+        Task
+        {
+            let straw = Straw()
+            await straw.write(input)
+            await straw.write(input)
+            let result = try await straw.read(maxSize: 10)
+
+            XCTAssertEqual(result.string, correct.string)
+        }
     }
 }
