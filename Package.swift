ed527fc5c4f7032283396d10fe5cb73e7ca0e010
bragelbytes <joebragel@gmail.com>
2023-10-02 13:47:26 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
 
-        .package(url: "https://github.com/OperatorFoundation/Chord.git", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Chord.git", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
