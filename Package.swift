cb34185dfd8037bc0ce89b735856464d1dd01ab8
bragelbytes <joebragel@gmail.com>
2023-12-14 17:38:43 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
 
-        .package(url: "https://github.com/OperatorFoundation/Chord.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
     ],
     targets: [
